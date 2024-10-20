f1a43718a070c3d8b6be5d19ac732469bd84941c
addala raed <addala.raed@gmail.com>
2024-10-20 19:17:45 +0100

    scaffold

--- backend/apps/backend/webpack.config.js
+++ backend/apps/backend/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/api'),
+    path: join(__dirname, '../../dist/apps/backend'),
   },
   plugins: [
     new NxAppWebpackPlugin({
@@ -12,7 +12,7 @@ module.exports = {
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
       assets: ['./src/assets'],
-      optimization: false,
+      optimization: true,
       outputHashing: 'none',
       generatePackageJson: true,
     }),
